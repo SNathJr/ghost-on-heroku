ccef87e52674c501efba69702b39f104096d125a
Swastik nath <swastik.nath@live.com>
2019-08-14 09:16:58 +0530

    add support for subdirs @cancan101

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var ghost = require('ghost');
+var express = require('express');
+var urlService = require('./node_modules/ghost/core/server/services/url');
+var parentApp = express();
 
 // Run a single Ghost process
 ghost()
-  .then( ghostServer => ghostServer.start() )
+  .then( ghostServer => {
+    // this is what you need to get subdirectories working properly!
+    parentApp.use(urlService.utils.getSubdir(), ghostServer.rootApp);
+    ghostServer.start(parentApp);
+  })
   .catch( error => {
     console.error(`Ghost server error: ${error.message} ${error.stack}`);
     process.exit(1);
